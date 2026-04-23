3b8a828259d1ba32ec5975daa3b62d9210fa18ef
Jonas Sørensen <js@firtal.com>
2026-04-23 14:05:18 +0200

    Use prebuilt arm64 binary for easier install

--- Formula/teams-luxafor-sync.rb
+++ Formula/teams-luxafor-sync.rb
@@ -1,14 +1,26 @@
 class TeamsLuxaforSync < Formula
   desc "Sync Luxafor LED color with Microsoft Teams presence on macOS"
   homepage "https://github.com/jonasrappy/teams-luxafor-presence-sync"
-  url "https://github.com/jonasrappy/teams-luxafor-presence-sync/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "b9bf8ac4f3d669c0d24a6a65a1a542828adefd9035f7e10813b5622c80a8575c"
   license "MIT"
+  version "0.2.0"
 
-  depends_on "go" => :build
+  on_arm do
+    url "https://github.com/jonasrappy/teams-luxafor-presence-sync/releases/download/v0.2.0/teams-luxafor-sync_darwin_arm64.tar.gz"
+    sha256 "fcbfefbd3b680c87ea26c1ab730cfb0616f1e599be8693fd7f4cb13e877e4e10"
+  end
+
+  on_intel do
+    url "https://github.com/jonasrappy/teams-luxafor-presence-sync/archive/refs/tags/v0.2.0.tar.gz"
+    sha256 "b9bf8ac4f3d669c0d24a6a65a1a542828adefd9035f7e10813b5622c80a8575c"
+    depends_on "go" => :build
+  end
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/teams-luxafor-sync"
+    if Hardware::CPU.arm?
+      bin.install "teams-luxafor-sync"
+    else
+      system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/teams-luxafor-sync"
+    end
   end
 
   service do
