26c2a63f2e5d8589f0da7be3cf3112a895b748f2
Jonas Sørensen <js@firtal.com>
2026-04-23 14:46:47 +0200

    teams-luxafor-sync: ship universal prebuilt assets v0.2.2

--- Formula/teams-luxafor-sync.rb
+++ Formula/teams-luxafor-sync.rb
@@ -1,25 +1,23 @@
 class TeamsLuxaforSync < Formula
   desc "Sync Luxafor LED color with Microsoft Teams presence on macOS"
   homepage "https://github.com/jonasrappy/teams-luxafor-presence-sync"
+  version "0.2.2"
   license "MIT"
-  version "0.2.1"
+  depends_on :macos
 
-  on_arm do
-    url "https://github.com/jonasrappy/teams-luxafor-presence-sync/releases/download/v0.2.1/teams-luxafor-sync_darwin_arm64.tar.gz"
-    sha256 "795c5d1e6b6b81f32beb9b810a5f621c7a6b69100122c5b7fd9aba92c850b244"
-  end
-
-  on_intel do
-    url "https://github.com/jonasrappy/teams-luxafor-presence-sync/archive/refs/tags/v0.2.1.tar.gz"
-    sha256 "c6317222bd2232fbdd24ff11e08108bcc4269247b21841e1c0d33d8fcc516bd5"
-    depends_on "go" => :build
+  if Hardware::CPU.arm?
+    url "https://github.com/jonasrappy/teams-luxafor-presence-sync/releases/download/v0.2.2/teams-luxafor-sync_darwin_arm64.tar.gz"
+    sha256 "a4aedde7e8051a9e5783fc959daabffd0d05b3311992da923ab98dafd101d858"
+  else
+    url "https://github.com/jonasrappy/teams-luxafor-presence-sync/releases/download/v0.2.2/teams-luxafor-sync_darwin_amd64.tar.gz"
+    sha256 "6ea3a6e0f56dc1269af39e768789f53a15a0f058da05c1f18dbdbdfceb73bed8"
   end
 
   def install
     if Hardware::CPU.arm?
-      bin.install "teams-luxafor-sync"
+      bin.install "teams-luxafor-sync_arm64" => "teams-luxafor-sync"
     else
-      system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/teams-luxafor-sync"
+      bin.install "teams-luxafor-sync_amd64" => "teams-luxafor-sync"
     end
   end
 
@@ -28,10 +26,22 @@ class TeamsLuxaforSync < Formula
     keep_alive true
     log_path var/"log/teams-luxafor-sync.log"
     error_log_path var/"log/teams-luxafor-sync-error.log"
-    environment_variables POLL_MS: "3000",
-                          TAIL_BYTES: "262144",
-                          FALLBACK_LOG_SCAN_COUNT: "5",
-                          REAPPLY_MS: "15000"
+    environment_variables(
+      POLL_MS:                 "3000",
+      TAIL_BYTES:              "262144",
+      FALLBACK_LOG_SCAN_COUNT: "5",
+      REAPPLY_MS:              "15000",
+    )
+  end
+
+  def caveats
+    <<~EOS
+      If Teams status does not sync, grant Full Disk Access to:
+        #{opt_bin}/teams-luxafor-sync
+
+      Then restart the service:
+        brew services restart teams-luxafor-sync
+    EOS
   end
 
   test do
